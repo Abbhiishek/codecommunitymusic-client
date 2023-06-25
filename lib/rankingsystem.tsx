f81fa15fe3d93b9bdd880aa8a292999fc70adca7
Abhishek Kushwaha <abhishekkushwaha1479@gmail.com>
2023-06-25 16:47:51 +0530

    feat: lib ranking system

--- lib/rankingsystem.tsx
+++ lib/rankingsystem.tsx
@@ -1,14 +1,15 @@
 const levelRanges: { min: number; max: number; name: string, color: string }[] = [
     { min: 0, max: 10, name: 'Beginners', color: 'text-green-500' },
     { min: 11, max: 50, name: 'Rogers', color: 'text-red-500' },
-    { min: 51, max: 150, name: 'Explorers', color: 'text-green-500' },
-    { min: 151, max: 400, name: 'Adventurers', color: 'text-teal-500' },
-    { min: 401, max: 900, name: 'Achievers', color: 'text-green-500' },
-    { min: 901, max: 6000, name: 'Challengers', color: 'text-green-500' },
-    { min: 6001, max: 9000, name: 'Masters', color: 'text-green-500' },
-    { min: 9000, max: 18000, name: 'Experts', color: 'text-green-500' },
-    { min: 18001, max: 35000, name: 'Legends', color: 'text-green-500' },
-    { min: 35001, max: Infinity, name: 'Titans', color: 'text-yellow-500' },
+    { min: 51, max: 100, name: 'Explorers', color: 'text-green-500' },
+    { min: 101, max: 200, name: 'Adventurers', color: 'text-teal-500' },
+    { min: 201, max: 350, name: 'Achievers', color: 'text-green-500' },
+    { min: 351, max: 500, name: 'Challengers', color: 'text-green-500' },
+    { min: 501, max: 650, name: 'Masters', color: 'text-green-500' },
+    { min: 651, max: 799, name: 'Experts', color: 'text-green-500' },
+    { min: 800, max: 1150, name: 'Legends', color: 'text-green-500' },
+    { min: 1151, max: 1500, name: 'Heroes', color: 'text-green-500' },
+    { min: 1501, max: 2000, name: 'Gods', color: 'text-green-500' },
 ];
 
 
