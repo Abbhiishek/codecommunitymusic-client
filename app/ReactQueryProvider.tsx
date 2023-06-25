74899fb7019aa9d5c97a3ec5131390bd038e6161
Abhishek Kushwaha <abhishekkushwaha1479@gmail.com>
2023-06-25 21:20:05 +0530

    feat: query client

--- app/ReactQueryProvider.tsx
+++ app/ReactQueryProvider.tsx
@@ -2,12 +2,19 @@
 
 import { QueryClient, QueryClientProvider, } from '@tanstack/react-query';
 
-
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            staleTime: Infinity,
+        },
+    }
+});
 
 
 export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
     return (
-        <QueryClientProvider client={new QueryClient()}>
+        <QueryClientProvider client={queryClient}>
             {children}
         </QueryClientProvider>
     )
