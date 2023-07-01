b2512a729d484be0afb911c8f0f59af12e4df4c7
Abhishek Kushwaha <abhishekkushwaha1479@gmail.com>
2023-07-01 23:37:00 +0530

    fix: update page.tsx

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -10,7 +10,7 @@ import { useState } from "react"
 import * as z from "zod"
 
 
-export const PostValidator = z.object({
+const PostValidator = z.object({
     title: z
         .string()
         .min(3, {
