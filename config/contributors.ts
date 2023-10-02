6649bb219aca602b26b9ad02b807f8ac14bc42d2
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-10-02 22:14:24 +0530

    build: contributors page
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- config/contributors.ts
+++ config/contributors.ts
@@ -2,6 +2,22 @@ import { IContributor } from '@/types/Contributors';
 
 export const Contributor: IContributor[] = [
   {
+    name: 'Debangan Paul Chowdhury',
+    role: 'Full Stack Developer',
+    contributions: [
+      {
+        title: 'Added an icon with text instead of the Login to Create Projects text',
+        description:
+          'Added an icon with text instead of the Login to Create Projects text',
+        issueNumber: 14,
+        prNumber: 16,
+        ScopeofChange: 'UI/UX',
+      },
+    ],
+    githubUsername: 'Debangan202Ok',
+    rewviewWords: 'hola! Happy Hacking 2023',
+  },
+  {
     name: 'Pratyush Singha',
     role: 'Frontend Developer',
     contributions: [
@@ -12,6 +28,14 @@ export const Contributor: IContributor[] = [
         issueNumber: 21,
         prNumber: 27,
         ScopeofChange: 'UI/UX',
+      },
+      {
+        title: 'docs: include local setup instructions in readme ',
+        description:
+          'Added Documentation for local setup',
+        issueNumber: 23,
+        prNumber: 26,
+        ScopeofChange: "Documentation",
       },
     ],
     githubUsername: 'pratyushsingha',
