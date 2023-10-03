4dd8798454e10734c2427437c7257a288769e094
pratyush Singha <pratyushsingha83@gmail.com>
2023-10-03 22:10:12 +0530

    refactor: updated the details to contributors.ts

--- config/contributors.ts
+++ config/contributors.ts
@@ -6,7 +6,8 @@ export const Contributor: IContributor[] = [
     role: 'Full Stack Developer',
     contributions: [
       {
-        title: 'Added an icon with text instead of the Login to Create Projects text',
+        title:
+          'Added an icon with text instead of the Login to Create Projects text',
         description:
           'Added an icon with text instead of the Login to Create Projects text',
         issueNumber: 14,
@@ -22,20 +23,19 @@ export const Contributor: IContributor[] = [
     role: 'Frontend Developer',
     contributions: [
       {
+        title: 'docs: include local setup instructions in readme ',
+        description: 'Added Documentation for local setup',
+        issueNumber: 23,
+        prNumber: 26,
+        ScopeofChange: 'Documentation',
+      },
+      {
         title: 'Enhance Password Strength Validation',
         description:
-          'Implemented a visual experience that shown user that resembles the password strength..Like a long strip which turns red,yellow,blue,green as user password is more secured',
+          'Implemented a visual experience that shown user that resembles the password strength,Like a long strip which turns red,yellow,blue,green as user password is more secured',
         issueNumber: 29,
         prNumber: 30,
         ScopeofChange: 'UI/UX',
-      },
-      {
-        title: 'docs: include local setup instructions in readme ',
-        description:
-          'Added Documentation for local setup',
-        issueNumber: 23,
-        prNumber: 26,
-        ScopeofChange: "Documentation",
       },
     ],
     githubUsername: 'pratyushsingha',
